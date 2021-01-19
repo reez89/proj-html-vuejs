d6d5e519bc8c783b4bc98268e73e29e519ffb8c6
Nicola R <nicola.rizzi89@hotmail.it>
2021-01-20 00:34:11 +0100

    Header + Vue
    I link e il logo dell' Header ora sono generati dinamicamente tramite Vue.

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+let app = new Vue({
+    el: '#app',
+    data:{
+        headerImage: './assets/img/gym_logo_1x.png',
+        navLinks:[
+            {
+                link : 'home',
+            },
+            {
+                link: 'about',
+            },
+            {
+                link: 'facilities',
+            },
+            {
+                link: 'membership',
+            },
+            {
+                link: 'testimonials',
+            },
+            {
+                link: 'blog'
+            }
+        ],
+    }
+})
