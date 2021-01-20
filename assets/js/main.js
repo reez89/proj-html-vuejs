95f79de49b8874a39476ff1a7babcccb38acd984
Nicola R <nicola.rizzi89@hotmail.it>
2021-01-20 13:28:57 +0100

    World results section
    Completate (da sistemare le schede);

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,6 +44,28 @@ let app = new Vue({
                 img:'./assets/img/divider-x-red.png',
                 text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.'
             }
+        ],
+        worldResults:[
+            {
+                img:`./assets/img/review_1-compressor.jpg`,
+                txt:`“The trainers at Avada Gym have helped me reach my fitness goals beyond anything I could of hoped for. I love it!”`,
+                author:`– Tara Smith`
+            },
+            {
+                img:`./assets/img/review_2-compressor.jpg`,
+                txt:`“The atmosphere at Avada Gym is amazing, the people are even better. All in all, it is a great place to work out.”`,
+                author:`– Simon Chel`
+            },
+            {
+                img:`./assets/img/review_3-compressor-2.jpg`,
+                txt:`“The facilities at Avada Gym stood out to me the most. They are well maintained and have high quality equipment.”`,
+                author:`– Jen Wirth`
+            },
+            {
+                img:`./assets/img/review_4-compressor-2.jpg`,
+                txt:`“No bad things to say about Avada Gym, they are top notch at every corner and truly take care of their customers.”`,
+                author:`– Jeff Glum`
+            },
         ]
     }
 })
